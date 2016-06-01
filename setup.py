3c4c1523949dbc51f473e88a388ce4a0f5a136d1
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-05-31 20:44:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 
+from setuptools import setup
+
+setup(name='email_format',
+      version='1.0',
+      description='A very simple package to detect email naming conventions based on provided emails',
+      url='http://github.com/killswitch-GUI/email_format',
+      author='Alexander Rymdeko-Harvey',
+      author_email='lolyearight@cybersyndicates.com',
+      license='GNU 3.0',
+      packages=['email_format'],
+      zip_safe=False)
